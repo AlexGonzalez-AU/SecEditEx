dc13c15fef76c5e3d204c3ba76452f15cc6cd2e7
Alex Gonzalez <gonzo_rhcp@hotmail.com>
2018-10-09 10:00:47 +1100

    Removed bad cha

--- Modules/Get-LocalGroupMember.psm1
+++ Modules/Get-LocalGroupMember.psm1
@@ -13,24 +13,24 @@ function ConvertTo-IdentityObject {
     }
         
     process {
-        if ($InputObject -is [Security.Principal.SecurityIdentifier]) {
+        if ($InputObject -is [Security.Principal.SecurityIdentifier]) {
             $sid = $InputObject
         }
-        elseif ($InputObject -is [string]) {
+        elseif ($InputObject -is [string]) {
             if ($InputObject.StartsWith("S-")) {
                 # S-1-5-21-2357950043-1680534590-2315089127-500
-                $sid = New-Object System.Security.Principal.SecurityIdentifier($InputObject)
+                $sid = New-Object System.Security.Principal.SecurityIdentifier($InputObject)
             }
             else {
                 # Administrator
                 $accountName = New-Object System.Security.Principal.NTAccount($InputObject)     
                 $sid = $accountName.Translate([System.Security.Principal.SecurityIdentifier])
             }
-        }
-        elseif($InputObject -is [byte[]]) {
+        }
+        elseif ($InputObject -is [byte[]]) {
             # @(1, 5, 0, 0, 0, 0, 0, 5, 21, 0, 0, 0, 91, 118, 139, 140, 62, 236, 42, 100, 231, 116, 253, 137, 244, 1, 0, 0)
-            $sid = New-Object System.Security.Principal.SecurityIdentifier($InputObject, 0)
-        }
+            $sid = New-Object System.Security.Principal.SecurityIdentifier($InputObject, 0)
+        }
         else {
             Write-Error -Message ("The InputObject of type '{0}' cannot be converted." -f $InputObject.GetType())
         }
